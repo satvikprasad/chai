f16c1c9e911906474be1c0b5fc18427f640d4843
Satvik Prasad <satvikprasad@gmail.com>
2023-12-06 21:46:22 +1100

    Update workflows

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 SET include=-Ilib\raylib\src
-SET linker=lib\raylib\src\libraylib.a
+SET linker=lib\raylib\src\libraylib.a gdi32.lib coredll.lib
 
 gcc src\*.c -o bin\chai %include% %linker%
