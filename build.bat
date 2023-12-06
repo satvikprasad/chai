5d26c348c11b3f1d570bcd8bf09e79a1c8ee4513
Satvik Prasad <satvikprasad@gmail.com>
2023-12-06 21:38:25 +1100

    Update workflows

--- build.bat
+++ build.bat
@@ -1,10 +1,6 @@
 @echo off
 
-SET INCLUDE_FLAGS=-ID:\lib\cglm\include -ID:\lib\curl\include -ID:\lib\raylib\raylib\src -ID:\lib\cimgui -ID:\lib\lua-5.4.6\src
-SET LINKER_FLAGS=D:\lib\raylib\raylib\projects\VS2022\build\raylib\bin\x64\Release.DLL\raylib.lib D:\lib\curl\lib\*.a D:\lib\cglm\build\Release\cglm.lib D:\lib\cimgui\Release\cimgui.lib D:\lib\lua-5.4.6\src\lua.lib
+SET include=-Ilib\raylib\src
+SET linker=-Ilib\raylib\src\libraylib.a
 
-if exist bin\chai.exe del bin\chai.exe
-
-cl src\*.c -o bin\chai.exe /DEBUG:FULL /Z7 %INCLUDE_FLAGS% %LINKER_FLAGS%
-
-if exist *.obj del *.obj
+gcc src\*.c -o bin\chai %include% %linker%
