212f286f73addfb511aab5a74c2bdb753fb269a1
Satvik Prasad <satvikprasad@gmail.com>
2023-12-07 17:05:09 +1100

    Fix windows build

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 SET include=-Ilib\raylib\src -Ilib\lua\src
-SET linker=lib\raylib\src\libraylib.a -lgdi32 -lole32 -loleaut32 -limm32 -lwinmm
+SET linker=lib\raylib\src\libraylib.a lib\lua\src\liblua.a -lgdi32 -lole32 -loleaut32 -limm32 -lwinmm
 
 gcc src\*.c -o bin\chai %include% %linker%
